39d9364bb75549a3daa5e0630363a1a7eee2351f
Mahamud Aden <mahamudadn@gmail.com>
2023-07-26 15:51:21 -0500

    imported react-chartjs library and dependencies

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -1,6 +1,32 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend
+);
+
 function Bar() {
 
    return(
