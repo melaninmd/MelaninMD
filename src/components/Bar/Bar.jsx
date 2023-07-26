2f2aaa527889a1ff71b995772cab57352f330524
Mahamud Aden <mahamudadn@gmail.com>
2023-07-26 15:49:22 -0500

    added a bar component

---
+++ src/components/Bar/Bar.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function Bar() {
+
+   return(
+    <>
+    
+    </>
+   )
+
+
+
+
+    
+}
+
+
+
+export default Bar
