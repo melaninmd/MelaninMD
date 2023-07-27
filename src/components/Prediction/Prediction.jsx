baf18d9db26bb16b2fd0bb7e3d000d2bd6e60d17
Mahamud Aden <mahamudadn@gmail.com>
2023-07-27 14:12:00 -0500

    returned my previous code

--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -1,51 +1,12 @@
-
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import './Prediction.css'
-function DiagnosisResult() {
-    const dispatch = useDispatch();
-    const conditionReducer = useSelector((store) => store.conditionReducer);
-   
-    function readMore (condition) {
-        window.location.href = condition.readMoreUrl;
-
-
-    }
-
 import { Bar } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
 
 function DiagnosisResult() {
   const conditionReducer = useSelector((store) => store.conditionReducer);
 
-
   const generateChartData = () => {
     const labels = conditionReducer.predictions?.map((condition) => condition.name) || [];
     const data = conditionReducer.predictions?.map((condition) => condition.confidence) || [];
-
-
-    return(
-        <>
-        <p>Result</p>
-        <img className="image-container" src={conditionReducer.url}/>
-        {conditionReducer.predictions?.map((condition, i) => {
-            return(
-                <div className="data-container">
-                    <b>{condition.name}</b> 
-                    <button onClick={()=> readMore(condition)}>Read More</button> 
-                   
-                    {/* <p>{condition.confidence}</p> */}
-                </div>
-               
-               
-
-            )
-           
-        })}
-        <div className="data-container">
-        <p>Acne</p>
-        <button src="https://www.loveshackfancy.com/collections/new-arrivals">read more</button>
-        </div>
 
     return {
       labels: labels,
@@ -58,7 +19,6 @@ function DiagnosisResult() {
       ],
     };
   };
-
 
   return (
     <>
