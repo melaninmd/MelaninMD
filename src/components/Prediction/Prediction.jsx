ca448d8b15d4a0cdd1ac22b13dea28dcd24dc6a5
Mahamud Aden <mahamudadn@gmail.com>
2023-07-27 12:18:20 -0500

    incorprated the bar chart into the prediction page and rendred

--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -1,30 +1,42 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { Bar } from 'react-chartjs-2';
+import { useSelector } from 'react-redux';
 
 function DiagnosisResult() {
-    const dispatch = useDispatch();
-    const conditionReducer = useSelector((store) => store.conditionReducer);
+  const conditionReducer = useSelector((store) => store.conditionReducer);
 
+  const generateChartData = () => {
+    const labels = conditionReducer.predictions?.map((condition) => condition.name) || [];
+    const data = conditionReducer.predictions?.map((condition) => condition.confidence) || [];
 
-    return(
-        <>
+    return {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Confidence',
+          data: data,
+          backgroundColor: ["green", "blue", "red", "yellow", "orange", "purple", "pink"], // Customize the bar color here
+        },
+      ],
+    };
+  };
+
+  return (
+    <>
+      <div>
         <p>Result</p>
-        <img src={conditionReducer.url}/>
-        {conditionReducer.predictions?.map((condition, i) => {
-            return(
-                <div>
-                   
-                    <p>{condition.name}</p> 
-                    <p>{condition.readMoreUrl}</p> 
-                    
-                </div>
-               
-               
-
-            )
-        })}
-
-        </>
-    )
+        <img src={conditionReducer.url} alt="Condition Image" />
+        {conditionReducer.predictions?.map((condition, i) => (
+          <div key={i}>
+            <p>{condition.name}</p>
+            <p>{condition.readMoreUrl}</p>
+          </div>
+        ))}
+      </div>
+      <div style={{ height: '300px', width: '400px' }}>
+        <Bar data={generateChartData()} />
+      </div>
+    </>
+  );
 }
-export default DiagnosisResult
+
+export default DiagnosisResult;
