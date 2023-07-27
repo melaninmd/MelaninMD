debad652c90c0242c08f0e13b344467efef0b76b
Mahamud Aden <mahamudadn@gmail.com>
2023-07-27 10:26:07 -0500

    changed condition reducer name, styled barchart

--- src/redux/reducers/condition.reducer.js
+++ src/redux/reducers/condition.reducer.js
@@ -2,7 +2,7 @@
 
 const conditionReducer = (state = [], action) => {
     switch(action.type) {
-        case 'SET_CATALOG':
+        case 'SET_CONDITION':
             return action.payload;
         default:
             return state;
