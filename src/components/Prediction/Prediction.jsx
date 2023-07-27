71c17f4b4823a184bd40cece47676082f0be346a
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-07-27 11:59:41 -0500

    switched readmore url to button gave prediction page dummy data for styling

--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -1,28 +1,39 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import './Prediction.css'
 function DiagnosisResult() {
     const dispatch = useDispatch();
     const conditionReducer = useSelector((store) => store.conditionReducer);
+   
+    function readMore (condition) {
+        window.location.href = condition.readMoreUrl;
+
+
+    }
 
 
     return(
         <>
         <p>Result</p>
-        <img src={conditionReducer.url}/>
+        <img className="image-container" src={conditionReducer.url}/>
         {conditionReducer.predictions?.map((condition, i) => {
             return(
-                <div>
-                   
+                <div className="data-container">
                     <p>{condition.name}</p> 
-                    <p>{condition.readMoreUrl}</p> 
-                    
+                    <button onClick={()=> readMore(condition)}>Read More</button> 
+                   
+                    {/* <p>{condition.confidence}</p> */}
                 </div>
                
                
 
             )
+           
         })}
+        <div className="data-container">
+        <p>acne</p>
+        <button src="https://www.loveshackfancy.com/collections/new-arrivals">read more</button>
+        </div>
 
         </>
     )
